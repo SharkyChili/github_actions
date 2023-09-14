5dcd33f11e4f3a4171a72060b00d7333f3c1e778
wayne <fw1036994377@outlook.com>
2023-09-14 15:25:48 +0800

    Update qywxRobot.py

--- qywxRobot.py
+++ qywxRobot.py
@@ -1,23 +1,29 @@
 import requests
 
-# 点餐助手
-request_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=dd18a2b8-94d8-4a59-a61a-f529fd60f964";
+def main():
+    # 点餐助手
+    request_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=dd18a2b8-94d8-4a59-a61a-f529fd60f964";
 
-# 请求头
-header = {
-    "Content-Type": "application/json"
-}
-
-# 请求体
-body = {
-    "msgtype" : "text",
-    "text" : {
-        "content" : "企微机器人测试，来自python"
+    # 请求头
+    header = {
+        "Content-Type": "application/json"
     }
-}
 
-# 发送post请求
-response = requests.post(url= request_url, json = body, headers= header)
+    # 请求体
+    body = {
+        "msgtype" : "text",
+        "text" : {
+            "content" : "企微机器人测试，来自python"
+        }
+    }
 
-# 打印结果
-print(response.json())
+    # 发送post请求
+    response = requests.post(url= request_url, json = body, headers= header)
+
+    # 打印结果
+    print(response.json())
+
+
+if __name__ == '__main__':
+    main()
+
